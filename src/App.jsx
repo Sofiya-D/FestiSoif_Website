df3cc033d96536aee6a6095c6e3a2ffda0624c28
Yassuiine <elmesbahyyassine080@gmail.com>
2025-12-02 14:48:54 +0100

    layouts update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import BottomBar from "./components/bar/BottomBar";
+import DetailsPage from "./pages/DetailsPage";
 
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50 text-gray-800">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+      <div className="flex flex-row h-screen w-screen">
+        <div className="h-[90%] w-full bg-gray-50 text-gray-800">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/details" element={<DetailsPage />} />
+          </Routes>
+        </div>
+        <BottomBar />
       </div>
-      <BottomBar />
     </BrowserRouter>
   );
 }
