df63690a1861f98b267af205727cc6b6b98448f0
Yassuiine <elmesbahyyassine080@gmail.com>
2025-12-02 16:40:56 +0100

    changes to details

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -8,16 +8,16 @@ function DetailsPage() {
         {"<"}
       </div>
       <div className='flex flex-col gap-2 bg-red-500 w-full h-full  items-center p-[5%] mt-10'>
-        <div className='flex flex-col justify-center items-center gap-y-6 px-2 w-full h-[30%] bg-yellow-200 rounded-md'>
-          <h3 className='bg-green-600 w-full font-extrabold text-2xl text-center text-white'>Point d'eau Zone B</h3>
-          <Droplets size={100}/>
+        <div className='flex flex-col justify-center items-center gap-y-6 px-2 w-full h-[30%] bg-linear-to-b from-[#363635] to-[#4568DC] rounded-md'>
+          <h3 className=' w-full font-extrabold text-2xl text-center text-white'>Point d'eau Zone B</h3>
+          <Droplets color='White'size={100}/>
         </div>
-        <div className='flex flex-col justify-center items-center px-2 w-full h-[25%] bg-yellow-200 rounded-md'>
+        <div className='flex flex-col justify-center gap-4 items-center px-2 w-full h-[25%] bg-linear-to-b from-[#4568DC] to-[#B06AB3] rounded-md'>
           <h2 className='text-white text-2xl font-extrabold'>120m</h2>
-          <h3>Faible</h3>
-          <h3>Proche de la scène principale</h3>
+          <h3 className='text-white text-xl font-bold'>Faible</h3>
+          <h3 className='text-white text-md font-extrabold opacity-80'>Proche de la scène principale</h3>
         </div>
-        <div className='w-full h-[25%] bg-yellow-200 mt-10'>
+        <div className='flex flex-colw-full h-[25%] bg-yellow-200 mt-10'>
           Hello
         </div>
 
